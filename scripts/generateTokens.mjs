cad020d26bfcad8d425f99fcb95cb61ef170a467
Diao <diaozheng999@gmail.com>
2023-06-15 22:43:14 +0800

    generate tokens

---
+++ scripts/generateTokens.mjs
@@ -0,0 +1,14 @@
+import  { writeFileSync } from "fs";
+
+for (let i = 0; i < 12; ++i) {
+  writeFileSync(
+    `assets/tokens/${i}`,
+    JSON.stringify({
+      image:
+        `https://raw.githubusercontent.com/diaozheng999/nft/master/assets/images/token-${i}.jpg`,
+      description:
+        "Thank you for coming to witness our wedding. May our love be strong and last forever.",
+      name: `Simon & Ya Min #${i}`,
+    })
+  );
+}
